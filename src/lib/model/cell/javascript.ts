b2852f45022b5f3a37bdf176c983efe8619ce817
Graeme Lockley <graeme.lockley@gmail.com>
2025-09-02 05:10:19 +0200

    feature: assign multiple variables for views

--- src/lib/model/cell/javascript.ts
+++ src/lib/model/cell/javascript.ts
@@ -7,6 +7,17 @@ export async function executeJavaScript(cell: ReactiveCell): Promise<void> {
 	if (pr.type === 'exception') {
 		cell.handleError(pr.exception);
 		return;
+	} else if (pr.type === 'assignment' && pr.viewof && pr.name !== null) {
+		const tmpName = cell.id;
+
+		cell.assignVariables([
+			{
+				name: pr.name,
+				dependencies: [tmpName, 'Generators'],
+				body: `Generators.input(${tmpName})`
+			},
+			{ name: tmpName, dependencies: pr.dependencies, body: pr.body }
+		]);
 	} else if (pr.type === 'assignment') {
 		const name = pr.name || cell.id;
 
